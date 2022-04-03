8c280ada2805b448b1182046834106c38d21d023
Gabriel Oliveira <greedwyblu@gmail.com>
2022-04-03 01:15:13 -0300

    sidebar

--- components/higiaAppPrincipal/Perfil/sidebar.tsx
+++ components/higiaAppPrincipal/Perfil/sidebar.tsx
@@ -5,17 +5,50 @@ import React, { useState } from 'react'
 import Image from 'next/image';
 import sidebar from '../../sidebar.module.scss'
 import { NextPage, GetStaticProps, GetStaticPaths, GetStaticPropsContext } from 'next'
+import { GoGear, GoPerson } from "react-icons/go";
+import { BsFillPersonBadgeFill, BsArrowBarLeft } from "react-icons/bs";
+import { FaBell, FaCameraRetro } from "react-icons/fa";
 
+import { usePerfilQuery } from '../../../graphql/generated/graphql';
 const  imgicon = require('../../assets/zyro-image_2_.ico');
 
 
 const PerfilPage:React.FC = ()=>{
+
+      const{data, error } = usePerfilQuery();
+ const ou =()=>{
+       return <div>{data?.perfil?.id}</div>
+ }
       const [show, setShow] = useState(false)
 
       const handleShow = () => {
           (show ? setShow(false) : setShow(true))
       }
-  
+      const [datas, setDatas] = useState([ou(), "hi there", "holla"]);
+
+      const [showAll, setShowAll] = useState(false);
+      const [currentIdx, setCurrentIdx] = useState(0);
+      const [showCurrent, setShowCurrent] = useState(false);
+    
+      const toggleAll = () => {
+        setShowAll(val => !val);
+        setShowCurrent(false);
+      };
+    
+      const toggleCurrent = () => {
+        if (!showCurrent) {
+          setShowCurrent(true);
+          setShowAll(false);
+          return;
+        }
+      };
+    
+      const setCurrent = index => {
+        setCurrentIdx(index);
+        toggleCurrent();
+      };
+      const [toggleViewMode, setToggleViewMode] = useState(false);
+      const contentClassname = toggleViewMode
       return (
       
      //@ts-nocheck
@@ -52,34 +85,46 @@ const PerfilPage:React.FC = ()=>{
     </div>
   
   </nav>
+  <div>
+      <div>
+        <button onClick={toggleAll}>{showAll ? "Hide All" : "Show All"}</button>
+        <button onClick={() => setCurrent(0)}>First</button>
+        <button onClick={() => setCurrent(1)}>Second</button>
+        <button onClick={() => setCurrent(2)}>Third</button>
+      </div>
+      <div>
+        {showAll && datas.map((el, i) => <p key={`content-${i}`}>{el}</p>)}
+      </div>
+
+      {showCurrent ? <div>{datas[currentIdx]}</div> : null}
+    </div>
+
+         
 
 <aside  className="w-64 h-404 max-h-full mt-6" aria-label="Sidebar"  >
 <div className="overflow-y-auto py-4 px-3 bg-transparent  ">
 <ul className="space-y-2">
-<li>
-<a href="#" className="flex items-center p-2 text-base font-normal hover:bg-slate-100 text-gray-900 border-r-4 border-transparent hover:border-emerald-500 transition duration-75">
-<svg className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
-<span className="ml-3">Dashboard</span>
-</a>
-</li>
 
-<li >
 
-<button onClick={handleShow} className="flex items-center hover:bg-slate-100 p-2 w-full text-base font-normal text-gray-900 border-r-4 border-transparent hover:border-emerald-500 transition duration-75 " aria-controls="dropdown-example" data-collapse-toggle="dropdown-example" >
-<svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd"></path></svg>
-<span className="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item="">E-commerce</span>
+<li className="mb-3">
+
+<button onClick={handleShow} className="flex items-center hover:bg-slate-100 p-2 w-full text-base font-normal text-gray-900 border-r-4 border-transparent hover:border-emerald-500 transition duration-75 mb-4" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example" >
+<span className="flex ml-1 text-left whitespace-nowrap" sidebar-toggle-item=""><GoGear className="mr-2 h-7 w-7 text-stone-400"/>Configuração</span>
 <svg sidebar-toggle-item="" className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
 </button>
 <div className={show ? "block" : "hidden"}>
 <ul id="submenu" className="  py-2 space-y-2">
 <li>
-<a href="#" className="flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 ">Products</a>
+<a href="#" className="flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900  transition duration-75 group hover:bg-gray-100 border-r-4 border-transparent hover:border-emerald-700 transition duration-75"><FaBell className="mr-2 h-7 w-7 text-stone-400"/>notificação</a>
 </li>
 <li>
-<a href="#" className="flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 ">Billing</a>
+<a  onClick={() => setToggleViewMode(!toggleViewMode)} className="flex items-center p-2 pl-11 w-full  text-base font-normal text-gray-900  transition duration-75 group hover:bg-gray-100 border-r-4 border-transparent hover:border-emerald-700 transition duration-75 "><FaCameraRetro className="mr-2 h-7 w-7 text-stone-400"/>post</a>
 </li>
 <li>
-<a href="#" className="flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 ">Invoice</a>
+<a href="#"  className="pl-11 w-full  flex items-center p-2 text-base font-normal hover:bg-slate-100 text-gray-900 border-r-4 border-transparent hover:border-emerald-700 transition duration-75  ">
+<span className="flex ml-1 whitespace-nowrap"><GoPerson className="mr-4 h-7 w-7 text-stone-400"/> cliente</span>
+
+</a>
 </li>
 </ul>
 </div>
@@ -87,44 +132,23 @@ const PerfilPage:React.FC = ()=>{
 </li>
 
 
-<li>
-<a href="#" className="flex items-center p-2 text-base hover:bg-slate-100 font-normal text-gray-900 border-r-4 border-transparent hover:border-emerald-500 transition duration-75">
-<svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path></svg>
-<span className="flex-1 ml-3 whitespace-nowrap">Kanban</span>
-<span className="inline-flex justify-center items-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span>
+
+<li className="mb-3">
+<a href="#" className="mb-3 mr-2 flex items-center p-2 text-base font-normal hover:bg-slate-100 text-gray-900 border-r-4 border-transparent hover:border-emerald-500 transition duration-75  ">
+<span className="flex ml-1 whitespace-nowrap"><GoPerson className="mr-4 h-7 w-7 text-stone-400"/> cliente</span>
 </a>
 </li>
-<li>
-<a href="#" className="flex items-center p-2 text-base font-normal hover:bg-slate-100 text-gray-900   border-r-4 border-transparent hover:border-emerald-500 transition duration-75">
-<svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path><path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path></svg>
-<span className="flex-1 ml-3 whitespace-nowrap">Inbox</span>
-<span className="inline-flex justify-center items-center p-3 ml-3 w-3 h-3 text-sm font-medium text-blue-600 bg-blue-200 rounded-full dark:bg-blue-900 dark:text-blue-200">3</span>
-</a>
-</li>
-<li>
-<a href="#" className="flex items-center p-2 text-base font-normal hover:bg-slate-100 text-gray-900 border-r-4 border-transparent hover:border-emerald-500 transition duration-75  ">
-<svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
-<span className="flex-1 ml-3 whitespace-nowrap">Users</span>
-</a>
-</li>
-<li>
+<li className="mb-4">
 <a href="#" className="flex items-center p-2 text-base font-normal hover:bg-slate-100 text-gray-900 border-r-4 border-transparent hover:border-emerald-500 transition duration-75">
-<svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd"></path></svg>
-<span className="flex-1 ml-3 whitespace-nowrap">Products</span>
+<span className="flex ml-1 whitespace-nowrap"><BsFillPersonBadgeFill className="mr-4 h-7 w-7 text-stone-400"/> profissional</span>
 </a>
 </li>
-<li>
+<li className="mb-3 mt-4">
 <a href="#" className="flex items-center p-2 text-base font-normal  hover:bg-slate-100 text-gray-900  border-r-4 border-transparent hover:border-emerald-500 transition duration-75">
-<svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clip-rule="evenodd"></path></svg>
-<span className="flex-1 ml-3 whitespace-nowrap">Sign In</span>
+<span className="flex ml-1 whitespace-nowrap"><BsArrowBarLeft className="mr-4 h-7 w-7 text-stone-400"/>  logout</span>
 </a>
 </li>
-<li>
-<a href="#" className="flex items-center p-2 text-base font-normal hover:bg-slate-100 text-gray-900  border-r-4 border-transparent hover:border-emerald-500 transition duration-75">
-<svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z" clip-rule="evenodd"></path></svg>
-<span className="flex-1 ml-3 whitespace-nowrap">Sign Up</span>
-</a>
-</li>
+
 </ul>
 </div>
 </aside>
